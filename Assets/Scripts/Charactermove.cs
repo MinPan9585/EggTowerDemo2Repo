a8c32503d1507af6b1643a0826f84928026d1134
eggtower1 <141388922+eggtower1@users.noreply.github.com>
2023-10-31 11:58:37 +0800

    10/31

--- Assets/Scripts/Charactermove.cs
+++ Assets/Scripts/Charactermove.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Charactermove: MonoBehaviour
-{
+   
+{ private Animator Anim;
+    public GameObject character;
+    void Awake()
+    {
+        Anim = GetComponent<Animator>();
+    }
     void Start()
     {
 
@@ -12,12 +18,35 @@ public class Charactermove: MonoBehaviour
     {
         Vector2 position = transform.position;
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.A))
         {
             position.x = position.x - 0.05f;
-        } else if (Input.GetKey("d")) 
+            character.transform.localScale = new Vector3(-1, 1, 0);
+
+
+        } else if (Input.GetKey(KeyCode.D)) 
         {
             position.x = position.x + 0.05f;
+            character.transform.localScale = new Vector3(1, 1, 0);
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Anim.SetBool("New Bool", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Anim.SetBool("New Bool", true);
+        }
+            if (Input.GetKeyUp(KeyCode.A))
+        {
+            Anim.SetBool("New Bool", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            
+            Anim.SetBool("New Bool", false);
         }
 
         transform.position = position;
